733521358c22d3f44596eb2d3b65b4fa3553b4bd
Dagramon <karamov-damir228@mail.ru>
2025-06-20 19:15:57 +0500

    Добавьте файлы проекта.

---
+++ zd5_karamov/Welcome.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CreditCalculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Welcome : ContentPage
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+        private void SignInButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(UsernameEntry.Text) || string.IsNullOrWhiteSpace(PasswordEntry.Text)) return;
+
+            var mainPage = new MainPage(UsernameEntry.Text);
+            Navigation.PushAsync(mainPage);
+            Navigation.RemovePage(this);
+
+        }
+    }
+}
